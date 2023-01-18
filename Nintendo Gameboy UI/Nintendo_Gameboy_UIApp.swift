8d6cb50c4ae90dadb89343358cea096cfc28f2c0
NT <usedforfunny@gmail.com>
2023-01-18 09:08:55 +0100

    Initial Commit

---
+++ Nintendo Gameboy UI/Nintendo_Gameboy_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nintendo_Gameboy_UIApp.swift
+//  Nintendo Gameboy UI
+//
+//  Created by Alexandre Talatinian on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nintendo_Gameboy_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
